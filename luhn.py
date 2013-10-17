3b0c56a0f39a61abeef45fddfb23466dffd82c68
Gregory F J Hogue <hogueonline@gmail.com>
2013-10-17 12:24:39 -0400

    Update and rename lehn.py to luhn.py

--- luhn.py
+++ luhn.py
@@ -1,80 +1,80 @@
-#School Assignment that deals with number validation using the Lehn Algorithm.
+#School Assignment that deals with number validation using the Luhn Algorithm.
 
 from os import system
 from sys import exit
 
 import pygame
 
-h = []
+
+def num(d):
+    global n
+    
+    while True
+        n = raw_input("Enter a " + str(d) + " Digit Number: ")
+        
+        if n.isdigit() and len(n) == d:
+            system("cls")
+            return True
+        system("cls")
+        print "[ERROR] Please Try Again.\n"
 
 def sin():
-    global a,c,n
-    c = 0
-    a = list(n[len(n) - 1::-1])
-    
-    for l in range(1, len(a),2):
-        a[l] = str(int(a[l]) * 2)
-        
-        if int(a[l]) > 9:
-            a[l] = str(int(a[l][0]) + int(a[l][1]))
-    
-    valid()
+    validate(list(n),1,0)
 
 def ccn():
-    global a,c,n
-    c = int(n[len(n) - 1])
-    a = list(n[len(n) - 2::-1])
+    validate(list(n[len(n)-2::-1]),0, int(n[len(n)-1]))
+
+def validate(a,m,c):
+    global h,n,t
     
-    for l in range(1, len(a),2):
+    for l in range(m, len(a),2):
         a[l] = str(int(a[l]) * 2)
         
         if int(a[l]) > 9:
             a[l] = str(int(a[l]) - 9)
     
-    
-    valid()
-
-def valid():
-    global a,c,n
-    t = 0
-    
     for l in range(0, len(a)):
         t = t + int(a[l])
     
-    if t % 10 == c:
+    if (10 - (t % 10) == c and m == 0) or t % 10 == c:
         print 'The Number, "' + n + '" is Valid.'
         h.append(n + ", Valid")
     else:
         print 'The Number, "' + n + '" is NOT Valid.'
         h.append(n + ", Invalid")
 
+
+h = []
+
 while True:
+    t = 0
+    
     while True:
         n = raw_input("Enter a 9 or 16 Digit Number: ")
-        
-        if(n.isdigit() and (9 <= len(n) <= 16)):
+        if(n.isdigit() and (len(n) == 9 or len(n) == 16)):
             system("cls")
             break
         
         system("cls")
         print "[ERROR] Please Try Again.\n"
     
-    if len(n) == 9:
-        sin()
-    else:
-        ccn()
-    
     while True:
-        print "\n1. Enter Another Number\n2. View History\n3. Exit\n"
+        print ("\n1. Enter A Credit Card Number\n2. Enter A Social Insurance" +
+            " Number\n5. View History\n6. Exit\n")
         menu = raw_input(">")
         system("cls")
         
         if menu == "1":
-            break
+            num(16)
+            ccn()
         elif menu == "2":
+            num(9)
+            sin()
+        elif menu == "5":
             print "[HISTORY]"
             for l in range(0, len(h)):
                 print str(l+1) + ". " + h[l] + "\n"
-        elif menu == "3":
+        elif menu == "6":
             exit()
+
 
