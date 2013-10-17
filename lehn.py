5ce5f3a3c7f811d4c4d0310dcf0b25bfd5a3ae45
Gregory F J Hogue <hogueonline@gmail.com>
2013-10-16 23:36:25 -0400

    still need to work out some issues

--- lehn.py
+++ lehn.py
@@ -1,14 +1,54 @@
 #School Assignment that deals with number validation using the Lehn Algorithm.
 
 from os import system
+from sys import exit
 
 import pygame
 
 h = []
 
-while True:
+def sin():
+    global a,c,n
+    c = 0
+    a = list(n[len(n) - 1::-1])
+    
+    for l in range(1, len(a),2):
+        a[l] = str(int(a[l]) * 2)
+        
+        if int(a[l]) > 9:
+            a[l] = str(int(a[l][0]) + int(a[l][1]))
+    
+    valid()
+
+def ccn():
+    global a,c,n
+    c = int(n[len(n) - 1])
+    a = list(n[len(n) - 2::-1])
+    
+    for l in range(1, len(a),2):
+        a[l] = str(int(a[l]) * 2)
+        
+        if int(a[l]) > 9:
+            a[l] = str(int(a[l]) - 9)
+    
+    
+    valid()
+
+def valid():
+    global a,c,n
     t = 0
     
+    for l in range(0, len(a)):
+        t = t + int(a[l])
+    
+    if t % 10 == c:
+        print 'The Number, "' + n + '" is Valid.'
+        h.append(n + ", Valid")
+    else:
+        print 'The Number, "' + n + '" is NOT Valid.'
+        h.append(n + ", Invalid")
+
+while True:
     while True:
         n = raw_input("Enter a 9 or 16 Digit Number: ")
         
@@ -20,45 +60,21 @@ while True:
         print "[ERROR] Please Try Again.\n"
     
     if len(n) == 9:
-        n = n + "0"
-        c = 0
-        a = list(n[len(n) - 1::-1])
-        n = n[:9]
+        sin()
     else:
-        c = int(n[len(n) - 1])
-        a = list(n[len(n) - 2::-1])
-
-    for l in range(0, len(a),2):
-        a[l] = str(int(a[l]) * 2)
-        
-        if len(a) == 9 and int(a[l]) >9:
-            a[l] = str(int(a[l][0]) + int(a[l][1]))
-        elif int(a[l]) > 9:
-            a[l] = str(int(a[l]) - 9)
-    
-    print a
-    
-    for l in range(0, len(a)):
-        t = t + int(a[l])
-    
-    if t % 10 == c:
-        print 'The Number, "' + n + '" is Valid.'
-        h.append(n + ", Valid")
-    else:
-        print 'The Number, "' + n + '" is NOT Valid.'
-        h.append(n + ", Invalid")
+        ccn()
     
     while True:
         print "\n1. Enter Another Number\n2. View History\n3. Exit\n"
         menu = raw_input(">")
         system("cls")
         
-        if menu == "1" or menu == "3":
+        if menu == "1":
             break
         elif menu == "2":
             print "[HISTORY]"
             for l in range(0, len(h)):
                 print str(l+1) + ". " + h[l] + "\n"
-    
-    if menu == "3":
-        break
+        elif menu == "3":
+            exit()
+
